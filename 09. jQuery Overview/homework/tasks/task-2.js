f40bc810867ee4eb226ab4c0a173eac9a6ae883e
Doncho Minkov <DonchoMinkov@gmail.com>
2015-07-28 14:31:20 +0300

    Added task 2 solution template

---
+++ 09. jQuery Overview/homework/tasks/task-2.js
@@ -0,0 +1,26 @@
+/* globals $ */
+
+/*
+Create a function that takes a selector and:
+* Finds all elements with class `button` or `content` within the provided element
+  * Change the content of all `.button` elements with "hide"
+* When a `.button` is clicked:
+  * Find the topmost `.content` element, that is before another `.button` and:
+    * If the `.content` is visible:
+      * Hide the `.content`
+      * Change the content of the `.button` to "show"       
+    * If the `.content` is hidden:
+      * Show the `.content`
+      * Change the content of the `.button` to "hide"
+    * If there isn't a `.content` element **after the clicked `.button`** and **before other `.button`**, do nothing
+* Throws if:
+  * The provided ID is not a **jQuery object** or a `string` 
+
+*/
+function solve() {
+  return function (selector) {
+    
+  };
+};
+
+module.exports = solve;
