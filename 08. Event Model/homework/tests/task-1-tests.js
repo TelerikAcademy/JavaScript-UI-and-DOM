dad8ad8b3d90eba6cd008d2a969eee8ca0072c0c
Doncho Minkov <DonchoMinkov@gmail.com>
2015-07-28 13:42:30 +0300

    Added more tests

--- 08. Event Model/homework/tests/task-1-tests.js
+++ 08. Event Model/homework/tests/task-1-tests.js
@@ -1,4 +1,4 @@
-/*globals describe, it, require, before, beforeEach, global, $*/
+/*globals describe, it, require, before, global, $*/
 var expect = require('chai').expect;
 var jsdom = require('jsdom');
 var jq = require('jquery');
@@ -64,6 +64,73 @@ describe('Task #1 Tests', function () {
       }
     });
 
+    it('expect to hide the next content on button click, and then show it again, when there are other elements', function () {
+      function createDummyNode(){
+        var tags = ['a', 'button', 'p', 'div', 'ul', 'li', 'ol', 'input', 'table','tr','br','hr', 'span'];
+        var node = document.createElement(tags[(Math.random*tags.length) | 0]);
+        node.innerHTML = 'Dummy Element: ' + Math.random();
+        return node;
+      }
+      var container = document.createElement('div'),
+        count = 150,
+        possibleTags = ['a', 'button', 'p', 'div'],
+        i,
+        buttonNode,
+        contentNode,
+        tag;
+
+      container.id = 'root';
+      var dymmyObjectChance = 70;
+      for (i = 0; i < count; i += 1) {
+        if(Math.random * 100 < dymmyObjectChance){
+          container.appendChild(createDummyNode());
+        }
+        tag = possibleTags[(Math.random() * possibleTags.length) | 0];
+        buttonNode = document.createElement(tag);
+        buttonNode.className = 'button';
+        if (i === ((count / 2) | 0 )) {
+          buttonNode.id = 'the-button';
+        }
+        container.appendChild(buttonNode);
+
+        possibleTags[(Math.random() * possibleTags.length) | 0];
+        contentNode = document.createElement(tag);
+        contentNode.className = 'content';
+
+        if(Math.random * 100 < dymmyObjectChance){
+          container.appendChild(createDummyNode());
+        }
+        container.appendChild(contentNode);
+
+        if(Math.random * 100 < dymmyObjectChance){
+          container.appendChild(createDummyNode());
+        }
+      }
+      document.body.innerHTML = container.outerHTML;
+
+      result('root');
+
+      var theButton = document.getElementById('the-button');
+
+      var theContent = theButton.nextElementSibling;
+      while (theContent && theContent.className.indexOf('content') < 0) {
+        theContent = theContent.nextElementSibling;
+      }
+
+      var event = document.createEvent('MouseEvents');
+      event.initMouseEvent('click', true, true);
+      theButton.dispatchEvent(event);
+
+      expect(theButton).to.exist;
+      expect(theContent).to.exist;
+      expect(theButton.innerHTML).to.equal('show');
+      expect(theContent.style.display).to.equal('none');
+      
+      theButton.dispatchEvent(event);      
+      expect(theButton.innerHTML).to.equal('hide');
+      expect(theContent.style.display).to.equal('');
+    });
+
     it('expect to hide the next content on button click, and then show it again', function () {
       var container = document.createElement('div'),
         count = 15,
@@ -78,7 +145,7 @@ describe('Task #1 Tests', function () {
         tag = possibleTags[(Math.random() * possibleTags.length) | 0];
         buttonNode = document.createElement(tag);
         buttonNode.className = 'button';
-        if (i === parseInt(count / 2)) {
+        if (i === ((count / 2) | 0 )) {
           buttonNode.id = 'the-button';
         }
         container.appendChild(buttonNode);
@@ -111,10 +178,37 @@ describe('Task #1 Tests', function () {
       theButton.dispatchEvent(event);      
       expect(theButton.innerHTML).to.equal('hide');
       expect(theContent.style.display).to.equal('');
-
-
+    });
+  });
+describe('Invalid', function () {
+    it('Expect to throw, when no params are provided', function () {
+      function test() {
+        result();
+      }
+      expect(test).to.throw();
+    });    
+    
+    /* Selector validation */
+    it('Expect to throw, when undefined is provided as selector', function () {
+      function test() {
+        result(undefined, []);
+      }
+      expect(test).to.throw();
     });
 
+    it('Expect to throw, when null is provided as selector', function () {
+      function test() {
+        result(null, []);
+      }
+      expect(test).to.throw();
+    });
+
+    it('Expect to throw, when selector that selects nothing is provided', function () {
+      function test() {
+        result('TEST_NO_SUCH_SELECTOR', []);
+      }
+      expect(test).to.throw();
+    });
   });
 
 });
