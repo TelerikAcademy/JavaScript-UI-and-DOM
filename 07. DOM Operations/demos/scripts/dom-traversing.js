f38fc469bbb43b3ebe42fd5b4ed79d05b949a209
Marin Jelev <marin.jelev92@gmail.com>
2015-07-30 17:42:19 +0300

    TypeError Illegal invocation fix
    TypeError Illegal invocation caused by var log = console.log; fixed.

--- 07. DOM Operations/demos/scripts/dom-traversing.js
+++ 07. DOM Operations/demos/scripts/dom-traversing.js
@@ -1,22 +1,24 @@
 (function () {
-	'use stict';
-	var list;
+    'use strict';
+    var list;
 
-	function printElementInfo(element) {
-		var log = console.log,
-			i;
-		log("-".repeat("100"));
-		log("Type of the node: " + element.nodeName);
-		log("Number of child nodes:" + element.childNodes.length);
-		log("child nodes: ");
-		for (i = 0, len = element.childNodes.length; i < len; i++) {
-			log("childNode at (" + i + ") position: " + element.childNodes[i].nodeName);
-		}
-		log("-".repeat("100"));
-	}
+    function printElementInfo(element) {
+        var i,
+            len;
 
-	list = document.getElementById("trainers-list");
-	printElementInfo(list);
+        console.log("-".repeat(100));
+        console.log("Type of the node: " + element.nodeName);
+        console.log("Number of child nodes:" + element.childNodes.length);
+        console.log("child nodes: ");
+        for (i = 0, len = element.childNodes.length; i < len; i++) {
+            console.log("childNode at (" + i + ") position: " + element.childNodes[i].nodeName);
+        }
+        console.log("-".repeat(100));
+    }
 
-	printElementInfo(document.body);
+    list = document.getElementById("trainers-list");
+
+    printElementInfo(list);
+
+    printElementInfo(document.body);
 }());
